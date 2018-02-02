49d7f6abdad9893e97483896aebf23c15a7f6314
Jennifer Pazos <jmp521@gmail.com>
2018-02-02 15:19:19 -0600

    added semantic ui css and created a navigation component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react'
-
+import { Input, Menu, Segment } from 'semantic-ui-react'
 
 class Nav extends Component {
 
   render() {
-    
+    return (
+      <Menu pointing>
+        <Menu.Item name="Home" />
+      </Menu>
+    )
   }
 }
+export default Nav
