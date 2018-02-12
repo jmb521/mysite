6e2cda29e308071693ad7427b825f921bb116be0
Jennifer Pazos <jmp521@gmail.com>
2018-02-12 02:11:53 -0600

    created mainheader, updated css

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav'
-
+import MainHeader from './components/MainHeader'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Nav />
+        <MainHeader />
       </div>
     );
   }
---
+++ src/components/MainHeader.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react'
+import {Grid} from 'semantic-ui-react'
+class MainHeader extends Component {
+
+  render() {
+    return (
+
+        <Grid className="background">
+        <Grid.Row />
+        <Grid.Row />
+          <Grid.Row>
+          <Grid.Column width={4} />
+          <Grid.Column width={8} textAlign={"center"}>
+
+              <h1 className="name">Jennifer Pazos</h1>
+              <p className="subheader">FullStack Web Developer</p>
+            </Grid.Column>
+
+            <Grid.Column width={4} />
+          </Grid.Row>
+          <Grid.Row />
+
+        </Grid>
+
+    )
+  }
+} export default MainHeader
