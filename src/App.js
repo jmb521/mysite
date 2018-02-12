0ac523f40042ee61868db0e5a6f61a084e11e3c0
Jennifer Pazos <jmp521@gmail.com>
2018-02-12 00:53:35 -0600

    created basic navigation using semantic-ui

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav'
 
+
 class App extends Component {
   render() {
     return (
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,28 @@
-import React, {Component} from 'react'
-import { Input, Menu, Segment } from 'semantic-ui-react'
+import React, { Component } from 'react'
+import { Menu, Segment, Grid } from 'semantic-ui-react'
 
 class Nav extends Component {
+  state = { activeItem: 'home' }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
   render() {
+    const { activeItem } = this.state
+
     return (
-      <Menu pointing>
-        <Menu.Item name="Home" />
-      </Menu>
+      <Segment inverted>
+      <Grid>
+      <Grid.Column width={2} />
+      <Grid.Column width={12} >
+        <Menu inverted pointing secondary>
+          <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+          <Menu.Item name='messages' active={activeItem === 'messages'} onClick={this.handleItemClick} />
+          <Menu.Item name='friends' active={activeItem === 'friends'} onClick={this.handleItemClick} />
+        </Menu>
+      </Grid.Column>
+      <Grid.Column width={2} />
+      </Grid>
+      </Segment>
     )
   }
-}
-export default Nav
+} export default Nav
